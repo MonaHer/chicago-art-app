c7a00077e3d8cd2b7b50f797cde9f0afc7a4fcd5
MonaHer <hertermona@gmail.com>
2023-10-06 11:29:41 +0200

    add pagination

--- src/features/artworks/ArtworksList.js
+++ src/features/artworks/ArtworksList.js
@@ -1,7 +1,9 @@
 import { useGetAllArtworksQuery } from "./apiSlice";
+import { useState } from "react";
 
 export default function ArtworksList() {
   const { data, isLoading, isError } = useGetAllArtworksQuery();
+  const [isVisible, setIsVisible] = useState(3);
 
   if (isError) {
     return <div>Error</div>;
@@ -11,14 +13,31 @@ export default function ArtworksList() {
     return <div>Loading..</div>;
   }
 
+  function showMoreArtworks() {
+    setIsVisible(isVisible + 3);
+  }
+
+  function showLessArtworks() {
+    setIsVisible(isVisible - 3);
+  }
+
   return (
     <>
       <h1>Artworks</h1>
       <ul>
-        {data.data.map((artwork) => (
+        {data.data.slice(0, isVisible).map((artwork) => (
           <h2 key={artwork.id}>{artwork.title}</h2>
         ))}
       </ul>
+      <button onClick={showLessArtworks} disabled={isVisible <= 3}>
+        Show Less
+      </button>
+      <button
+        onClick={showMoreArtworks}
+        disabled={isVisible >= data.data.length}
+      >
+        Show More
+      </button>
     </>
   );
 }
